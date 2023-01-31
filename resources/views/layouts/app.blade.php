fc1e10feff1920f7a2d1c1e0bbac6ea7ca681f63
Oleh Hrokalo <oleh.hrokalo@linemedia.com>
2023-01-31 17:58:21 +0200

    add template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/mediumish.css')}}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<!-- Begin Nav
+================================================== -->
+@include('layouts.navigation')
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+<!-- Page Heading -->
+@if (isset($header))
+    {{ $header }}
+@endif
+<!-- End Nav
+================================================== -->
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+<!-- Begin Site Title
+================================================== -->
+<div class="container">
+    <!-- End Site Title
+    ================================================== -->
+    {{$slot}}
+    <!-- Begin Footer
+    ================================================== -->
+    <div class="footer">
+        <p class="pull-left">
+            Copyright &copy; 2017 Your Website Name
+        </p>
+        <p class="pull-right">
+            Mediumish Theme by <a target="_blank" href="https://www.wowthemes.net">WowThemes.net</a>
+        </p>
+        <div class="clearfix">
         </div>
-    </body>
+    </div>
+    <!-- End Footer
+    ================================================== -->
+
+</div>
+<!-- /.container -->
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/ie10-viewport-bug-workaround.js')}}"></script>
+</body>
 </html>
